96801f5413bb743db2f845e570e5b64185bd7ef4
TobiasOnandia <tobiasonandia0@gmail.com>
2025-01-02 16:54:36 -0300

    fix: improve code formatting and consistency in Signup component

--- app/components/pages/Signup.tsx
+++ app/components/pages/Signup.tsx
@@ -1,25 +1,26 @@
-'use client';
+"use client";
 
-import { useActionState } from 'react';
-import { GoogleIcon } from '@components/common/Icons';
-import Link from 'next/link';
-import { toast } from 'sonner';
-import { supabase } from '@/app/lib/supabaseClient';
-import { useTransitionRouter } from 'next-view-transitions';
+import { useActionState } from "react";
+import { GoogleIcon } from "@components/common/Icons";
+import Link from "next/link";
+import { toast } from "sonner";
+import { supabase } from "@/app/lib/supabaseClient";
+import { useTransitionRouter } from "next-view-transitions";
 
 export const Signup = () => {
   const router = useTransitionRouter();
   const handleLogin = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: 'google',
-    })
+      provider: "google",
+    });
 
     if (error) {
-      console.error('Error registerd in:', error.message)
+      console.error("Error registered in:", error.message);
     } else {
-      console.log('registed in data:', data)
+      console.log("Registered in data:", data);
     }
-  }
+  };
+
   const [error, submitAction, isPending] = useActionState(
     async (previousState: { error: string } | null, formData: FormData) => {
       const email = formData.get("email");
@@ -29,8 +30,8 @@ export const Signup = () => {
       const lastName = formData.get("lastName");
 
       if (password !== confirmPassword) {
-        toast.error('Las contraseñas no coinciden.');
-        return { error: 'Las contraseñas no coinciden.' };
+        toast.error("Las contraseñas no coinciden.");
+        return { error: "Las contraseñas no coinciden." };
       }
 
       const { error: signupError } = await supabase.auth.signUp({
@@ -40,8 +41,8 @@ export const Signup = () => {
           data: {
             first_name: firstName,
             last_name: lastName,
-          }
-        }
+          },
+        },
       });
 
       if (signupError) {
@@ -49,23 +50,28 @@ export const Signup = () => {
         return { error: signupError.message };
       }
 
-      toast.success('Cuenta creada exitosamente.');
-      toast.success('Por favor verifica tu correo electrónico para activar tu cuenta.');
-      router.push('/');
+      toast.success("Cuenta creada exitosamente.");
+      toast.success("Por favor verifica tu correo electrónico para activar tu cuenta.");
+      router.push("/");
       return null;
     },
     null
   );
 
   return (
-    <main className="h-screen flex items-center justify-center bg-gray-100 py-8 px-4">
-      <div className="flex flex-col items-center bg-white p-8 md:p-10 rounded-lg shadow-lg border border-gray-200 w-full max-w-md">
-        <h1 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800">Crear Cuenta</h1>
-        {error && <p className="text-red-500 mb-4">{error.error}</p>}
+    <main className="min-h-screen flex items-center  justify-center bg-gray-100 py-8 px-4">
+      <div className="flex flex-col mt-16 lg:mt-0 items-center bg-white p-6 md:p-10 rounded-lg shadow-lg border border-gray-200 w-full max-w-lg md:max-w-xl lg:max-w-2xl">
+        <h1 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800 text-center">
+          Crear Cuenta
+        </h1>
+        {error && <p className="text-red-500 mb-4 text-center">{error.error}</p>}
 
-        <form action={submitAction} className="w-full grid grid-cols-1 sm:grid-cols-2 gap-2 space-y-5">
+        <form
+          action={submitAction}
+          className="w-full md:grid  grid-cols-1 flex flex-col md:grid-cols-2 gap-4"
+        >
           {/* First Name */}
-          <fieldset>
+          <fieldset className="col-span-1">
             <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
               Nombre
             </label>
@@ -80,7 +86,7 @@ export const Signup = () => {
           </fieldset>
 
           {/* Last Name */}
-          <fieldset>
+          <fieldset className="col-span-1">
             <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
               Apellido
             </label>
@@ -95,7 +101,7 @@ export const Signup = () => {
           </fieldset>
 
           {/* Email */}
-          <fieldset className='col-span-2'>
+          <fieldset className="col-span-2">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               Correo Electrónico
             </label>
@@ -110,7 +116,7 @@ export const Signup = () => {
           </fieldset>
 
           {/* Password */}
-          <fieldset>
+          <fieldset className="col-span-1">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
               Contraseña
             </label>
@@ -125,7 +131,7 @@ export const Signup = () => {
           </fieldset>
 
           {/* Confirm Password */}
-          <fieldset>
+          <fieldset className="col-span-1">
             <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
               Confirmar Contraseña
             </label>
@@ -142,7 +148,7 @@ export const Signup = () => {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full col-span-2 cursor-pointer  py-3 text-white bg-foreground rounded-lg shadow-md hover:bg-secondary transition-colors focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+            className="w-full col-span-2 cursor-pointer py-3 text-white bg-foreground rounded-lg shadow-md hover:bg-secondary transition-colors focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
           >
             Registrarse
           </button>
@@ -157,18 +163,17 @@ export const Signup = () => {
 
         {/* Google Signup */}
         <button
-          className="flex items-center justify-center cursor-pointer gap-2  w-full py-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-300"
+          className="flex items-center justify-center cursor-pointer gap-2 w-full py-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-300"
           onClick={handleLogin}
         >
           <GoogleIcon />
-
-          {isPending ? 'Registrarse con Google...' : 'Registrarse con Google'}
+          {isPending ? "Registrarse con Google..." : "Registrarse con Google"}
         </button>
 
         {/* Login Redirect */}
         <div className="mt-4 text-center">
           <p className="text-sm text-gray-600">
-            ¿Ya tienes una cuenta?{' '}
+            ¿Ya tienes una cuenta?{" "}
             <Link href="/login" className="text-primary font-medium hover:underline">
               Inicia sesión aquí
             </Link>
